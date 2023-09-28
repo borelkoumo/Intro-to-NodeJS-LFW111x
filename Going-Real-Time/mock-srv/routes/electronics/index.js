5a3499cc3d6e83fbc0ee7d6617c559f8206c0dd0
Borel KOUMO <borelkoumo@gmail.com>
2023-09-28 17:31:06 +0200

    Going real time

---
+++ Going-Real-Time/mock-srv/routes/electronics/index.js
@@ -0,0 +1,27 @@
+"use strict"
+const data = [
+  {
+    id: "A1",
+    name: "Vacuum Cleaner",
+    rrp: "99.99",
+    info: "The suckiest vacuum in the world.",
+  },
+  {
+    id: "A2",
+    name: "Leaf Blower",
+    rrp: "303.33",
+    info: "This product will blow your socks off.",
+  },
+]
+
+export default async function (fastify, opts) {
+  fastify.get('/', async (request, reply) => {
+    console.log('opts GET = ', opts)
+    return data
+  })
+  fastify.post("/", async function (request, reply) {
+    console.log('opts POST = ', opts)
+    fastify.mockDataInsert(request, opts.prefix.slice(1), data);
+    return data
+  });
+}
